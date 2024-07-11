b5bd14b1ebf9781b149d66f87e3f49b6872843cd
Pieter-JanLavaerts <pieterjan.lavaerts@gmail.com>
2024-07-11 17:24:28 +0200

    put book name in variable, and create pages folder

--- pageToPng.sh
+++ pageToPng.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
-pdftk Basic\ Mathematics\ \(Serge\ Lang\)\ \(Z-Library\).pdf cat $(($1 + $3)) output $1.pdf
+#!/bin/bash
+book="Basic Mathematics (Serge Lang).pdf"
+pdftk $book cat $(($1 + $3)) output $1.pdf
 pdftoppm -png $1.pdf > pages/$1.png
 page=$(printf "%03d" $1)
+mkdir pages
 cp pages/$1.png "named/$page $2.png"
 
