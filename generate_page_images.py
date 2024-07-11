1b182724c10b2108d9866b89d8e47d30df3c526d
Pieter-JanLavaerts <pieterjan.lavaerts@gmail.com>
2024-07-11 18:10:41 +0200

    fleshed out things a bit more

--- generate_page_images.py
+++ generate_page_images.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 from sys import argv
-from os import system
+import subprocess
 
 for l in open(argv[2]):
     try:
         ws = l.split(",")
         name = ws[0].replace(" ", "_").replace("(", "").replace(")", "").replace("/", "\\").replace("&", "")
         page = str(int(ws[1]))
-        cmd = "./pageToPng.sh " + argv[1].replace(" ", "\\ ").replace("(", "\\(").replace(")", "\\)") + " " + page + " " + name + " " + argv[3]
-        system(cmd)
+        cmd = ["./pageToPng.sh", argv[1], page, name, argv[3]]
+        print(cmd)
+        subprocess.run(cmd)
     except Exception as e:
         print("Exception on line: ", l)
         print(e)
